32292c230c6b28254c5415d822f1b0eb7fb76ec6
Sebastian Wozniewski <sebastian.wozniewski@kit.edu>
2020-05-14 12:48:14 +0200

    update scripts to 14 node training

--- dump.py
+++ dump.py
@@ -8,22 +8,23 @@ tag = sys.argv[1] if len(sys.argv)>=2 else "default"
 path_to_logs = sys.argv[2] if len(sys.argv)==3 else "output/log"
 print "Searching tag '" + tag + "' in " + path_to_logs
 
-eras = ["2016", "2017", "2018", "combined"]
+eras = ["2016", "2017", "2018", "all"]
 channels = ["em", "et", "mt", "tt", "cmb"]
 stages = ["inclusive", "stxs_stage0", "stxs_stage1p1"]
 
 POIdict = {
     "inclusive" : ["r"],
     "stxs_stage0" : ["r_ggH", "r_qqH"],
-    "stxs_stage1p1" : ["r_ggH_GG2H_0J",
+    "stxs_stage1p1" : ["r_ggH_GG2H_0J_PTH_0_10",
+                       "r_ggH_GG2H_0J_PTH_GT10",
                        "r_ggH_GG2H_PTH_GT200",
-                       "r_ggH_GG2H_1J_PTH_0_120",
+                       "r_ggH_GG2H_1J_PTH_0_60",
+                       "r_ggH_GG2H_1J_PTH_60_120",
                        "r_ggH_GG2H_1J_PTH_120_200",
-                       "r_ggH_GG2H_GE2J_MJJ_0_350",
-                       "r_xxH_QQ2HQQ_GE2J_MJJ_350_700_PTH_0_200",
-                       "r_xxH_QQ2HQQ_GE2J_MJJ_GT700_PTH_0_200",
-                       "r_qqH_QQ2HQQ_LE1J",
-                       "r_qqH_QQ2HQQ_GE2J_MJJ_0_350",
+                       "r_ggH_GG2H_GE2J",
+                       "r_qqH_QQ2HQQ_GE2J_MJJ_350_700_PTH_0_200",
+                       "r_qqH_QQ2HQQ_GE2J_MJJ_GT700_PTH_0_200",
+                       "r_qqH_QQ2HQQ_noVBFtopo",
                        "r_qqH_QQ2HQQ_GE2J_MJJ_GT350_PTH_GT200"]
     }
     
@@ -36,7 +37,9 @@ for era in eras:
     for channel in channels:
         fitresults[era][channel] = {}
         for stage in stages:
+            print "Searching file %s/signal-strength-%s-%s-%s-%s.log"%(path_to_logs, era, tag, channel, stage)
             if os.path.isfile(os.path.join(path_to_logs, "signal-strength-%s-%s-%s-%s.log"%(era, tag, channel, stage))):
+                print "Found file signal-strength-%s-%s-%s-%s.log"%(era, tag, channel, stage)
                 with open(os.path.join(path_to_logs, "signal-strength-%s-%s-%s-%s.log"%(era, tag, channel, stage))) as infile:
                     lines=infile.read()
                 lines=lines.split("\n")
@@ -75,36 +78,39 @@ f.write("\\end{tabular}\n} % end footnotesize\n\\end{center}\n\\end{table}\n")
 for era in eras:
     f.write("\\begin{table}[!h]\n\\begin{center}{\\footnotesize\n\\begin{tabular}{|l|r|}\n\\hline\nSTXS stage 1 category & signal strength \\\\\n\\hline\n")
     
+    f.write("\hline\nVBF:&\\\\\n")
+
     f.write("VBF topology $350\,\mathrm{GeV}<m_{jj}<700\,\mathrm{GeV}$                                      & $")
-    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_xxH_QQ2HQQ_GE2J_MJJ_350_700_PTH_0_200"][0], fitresults[era][channel]["r_xxH_QQ2HQQ_GE2J_MJJ_350_700_PTH_0_200"][1], fitresults[era][channel]["r_xxH_QQ2HQQ_GE2J_MJJ_350_700_PTH_0_200"][2]))
+    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_qqH_QQ2HQQ_GE2J_MJJ_350_700_PTH_0_200"][0], fitresults[era][channel]["r_qqH_QQ2HQQ_GE2J_MJJ_350_700_PTH_0_200"][1], fitresults[era][channel]["r_qqH_QQ2HQQ_GE2J_MJJ_350_700_PTH_0_200"][2]))
     
     f.write("VBF topology $m_{jj}>700\,\mathrm{GeV}$                                                        & $")
-    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_xxH_QQ2HQQ_GE2J_MJJ_GT700_PTH_0_200"][0], fitresults[era][channel]["r_xxH_QQ2HQQ_GE2J_MJJ_GT700_PTH_0_200"][1], fitresults[era][channel]["r_xxH_QQ2HQQ_GE2J_MJJ_GT700_PTH_0_200"][2]))
+    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_qqH_QQ2HQQ_GE2J_MJJ_GT700_PTH_0_200"][0], fitresults[era][channel]["r_qqH_QQ2HQQ_GE2J_MJJ_GT700_PTH_0_200"][1], fitresults[era][channel]["r_qqH_QQ2HQQ_GE2J_MJJ_GT700_PTH_0_200"][2]))
     
-    f.write("\hline\nVBF:&\\\\\n")
-    
-    f.write("0, 1 jets                                                                                      & $")
-    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_qqH_QQ2HQQ_LE1J"][0], fitresults[era][channel]["r_qqH_QQ2HQQ_LE1J"][1], fitresults[era][channel]["r_qqH_QQ2HQQ_LE1J"][2]))
-    
-    f.write("$m_{jj}<350\,\mathrm{GeV}$                                                                     & $")
-    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_qqH_QQ2HQQ_GE2J_MJJ_0_350"][0], fitresults[era][channel]["r_qqH_QQ2HQQ_GE2J_MJJ_0_350"][1], fitresults[era][channel]["r_qqH_QQ2HQQ_GE2J_MJJ_0_350"][2]))
+    f.write("no VBF topology                                                                                & $")
+    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_qqH_QQ2HQQ_noVBFtopo"][0], fitresults[era][channel]["r_qqH_QQ2HQQ_noVBFtopo"][1], fitresults[era][channel]["r_qqH_QQ2HQQ_noVBFtopo"][2]))
     
     f.write("$p_T^{H}>200\,\mathrm{GeV}$                                                                    & $")
     f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_qqH_QQ2HQQ_GE2J_MJJ_GT350_PTH_GT200"][0], fitresults[era][channel]["r_qqH_QQ2HQQ_GE2J_MJJ_GT350_PTH_GT200"][1], fitresults[era][channel]["r_qqH_QQ2HQQ_GE2J_MJJ_GT350_PTH_GT200"][2]))
     
     f.write("\hline\nGluon fusion:&\\\\\n")
     
-    f.write("0 jets, $p_T^{H}\in[0,\,200]\,\mathrm{GeV}$                                                    & $")
-    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_ggH_GG2H_0J"][0], fitresults[era][channel]["r_ggH_GG2H_0J"][1], fitresults[era][channel]["r_ggH_GG2H_0J"][2]))
+    f.write("0 jets, $p_T^{H}\in[0,\,10]\,\mathrm{GeV}$                                                    & $")
+    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_ggH_GG2H_0J_PTH_0_10"][0], fitresults[era][channel]["r_ggH_GG2H_0J_PTH_0_10"][1], fitresults[era][channel]["r_ggH_GG2H_0J_PTH_0_10"][2]))
+
+    f.write("0 jets, $p_T^{H}\in[10,\,200]\,\mathrm{GeV}$                                                    & $")
+    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_ggH_GG2H_0J_PTH_GT10"][0], fitresults[era][channel]["r_ggH_GG2H_0J_PTH_GT10"][1], fitresults[era][channel]["r_ggH_GG2H_0J_PTH_GT10"][2]))
     
-    f.write("1 jet, $p_T^{H}\in[0,\,120]\,\mathrm{GeV}$                                                     & $")
-    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_ggH_GG2H_1J_PTH_0_120"][0], fitresults[era][channel]["r_ggH_GG2H_1J_PTH_0_120"][1], fitresults[era][channel]["r_ggH_GG2H_1J_PTH_0_120"][2]))
+    f.write("1 jet, $p_T^{H}\in[0,\,60]\,\mathrm{GeV}$                                                     & $")
+    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_ggH_GG2H_1J_PTH_0_60"][0], fitresults[era][channel]["r_ggH_GG2H_1J_PTH_0_60"][1], fitresults[era][channel]["r_ggH_GG2H_1J_PTH_0_60"][2]))
+
+    f.write("1 jet, $p_T^{H}\in[60,\,120]\,\mathrm{GeV}$                                                     & $")
+    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_ggH_GG2H_1J_PTH_60_120"][0], fitresults[era][channel]["r_ggH_GG2H_1J_PTH_60_120"][1], fitresults[era][channel]["r_ggH_GG2H_1J_PTH_60_120"][2]))
     
     f.write("1 jet, $p_T^{H}\in[120,\,200]\,\mathrm{GeV}$                                                   & $")
     f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_ggH_GG2H_1J_PTH_120_200"][0], fitresults[era][channel]["r_ggH_GG2H_1J_PTH_120_200"][1], fitresults[era][channel]["r_ggH_GG2H_1J_PTH_120_200"][2]))
     
-    f.write("$\ge2$ jets, $p_T^{H}\in[0,\,200]\,\mathrm{GeV}$, $m_{jj}<350\,\mathrm{GeV}$                   & $")
-    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_ggH_GG2H_GE2J_MJJ_0_350"][0], fitresults[era][channel]["r_ggH_GG2H_GE2J_MJJ_0_350"][1], fitresults[era][channel]["r_ggH_GG2H_GE2J_MJJ_0_350"][2]))
+    f.write("$\ge2$ jets, $p_T^{H}\in[0,\,200]\,\mathrm{GeV}$                                               & $")
+    f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_ggH_GG2H_GE2J"][0], fitresults[era][channel]["r_ggH_GG2H_GE2J"][1], fitresults[era][channel]["r_ggH_GG2H_GE2J"][2]))
     
     f.write("$p_T^{H}>\,200\,\mathrm{GeV}$                                                                  & $")
     f.write("%s\\substack{%s \\\\ %s}$\\\\\n"%(fitresults[era][channel]["r_ggH_GG2H_PTH_GT200"][0], fitresults[era][channel]["r_ggH_GG2H_PTH_GT200"][1], fitresults[era][channel]["r_ggH_GG2H_PTH_GT200"][2]))
